8c61af7356915b013203da73e5703dfa33fa10eb
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-14 22:49:24 +0530

    header complere

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Home from "./pages/home/Home";
 import Topbar from "./topbar/Topbar";
 
 function App() {
   return (
     <>
       <Topbar />;
+      <Home />
     </>
   );
 }
---
+++ client/src/header/Header.jsx
@@ -0,0 +1,19 @@
+import "./header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitlesm">React & Note</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+        srcset=""
+      />
+    </div>
+  );
+};
+
+export default Header;
