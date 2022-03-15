630d4189658f7a5a653ba0f6d844b5c1cb532848
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-15 17:21:55 +0530

    mongodb connection

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("MongoDB connected through Mongoose"))
+  .catch((err) => console.log(err));
+app.listen("5000", () => {
+  console.log("Backend is Running");
+});
